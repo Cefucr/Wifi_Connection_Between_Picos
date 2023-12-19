9c76224834b4b31a4c5e2f1ff78138f0401fb39c
Väinö Jämsä <105347805+Cefucr@users.noreply.github.com>
2023-12-19 14:08:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,28 @@
-from machine import Pin,ADC
-import network
-import time
 import socket
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect('ssid', 'pin')
-while not wlan.isconnected() and wlan.status() >= 0:
-    print("Waiting to connect:")
-    time.sleep(1)
-wlan.status()
-wlan.ifconfig()
-print("IP = " + str(wlan.ifconfig()[0]))
-inpu = input("Receive or send[R/S]: ")
+#For server side
+#s = socket.socket()
+#s.bind(("0.0.0.0", 80))
+#s.listen()
+#(cl, addr) = s.accept()
 
-def receivevalue():
-    print("Receiving...")
-    ai = socket.getaddrinfo("ip", 80) #The other devices ip address
-    addr = ai[0][-1]
-    s = socket.socket()
-    s.connect(addr)
-    s.send(b"GET Data")
-    ss=str(s.recv(512))
-    s.close()
-    time.sleep(0.2)
-    
-    return ss
+#For client side
+#cl = socket.socket()
+#cl.connect((ip,'port'))
 
-def sendvalue(val):
-    print("Sending...")
-    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-    s = socket.socket()
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(addr)
-    s.listen(1)
-    try:
-        cl, addr = s.accept()
-        request = cl.recv(1024)
-        cl.send(str(val))
-        print("Sent " + str(val))
-        cl.close()
-    except OSError as e:
-        cl.close
-        print("Error Connection Terminated")
-        
-if inpu == "R":
-    received = str(receivevalue())
-    print("Got " + received[1:])
-elif inpu == "S":
-    while True:
-        sendvalue("value")
+
+def senddata(data):
+    cl.send(data)
+
+def receivedata():
+    return cl.recv(1024)
+
+
+inp = input("What do? Send or Receive(S/R): ")
+
+if(inp == "S"):
+    senddata("lahetetty")
+elif(inp == "R"):
+    print("Got: " + str(receivedata()))
 else:
-    print("Invalid input. Please try again.")
+    print("Dumbass")
