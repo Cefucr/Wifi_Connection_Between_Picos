812e41e36eea29244637c8e8585fe6ef82549047
Väinö Jämsä <105347805+Cefucr@users.noreply.github.com>
2023-12-19 13:28:06 +0200

    Update alt_main.py

--- alt_main.py
+++ alt_main.py
@@ -9,47 +9,36 @@ wlan.connect('surffi', '')
 while not wlan.isconnected() and wlan.status() >= 0:
     print("Waiting to connect:")
     time.sleep(1)
+    
 wlan.status()
 wlan.ifconfig()
-print("IP = " + str(wlan.ifconfig()[0]))
-inpu = input("Receive or send[R/S]: ")
 
-
-addr = socket.getaddrinfo('192.168.50.12', 80)[0][-1]
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 s = socket.socket()
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(addr)
 s.listen(1)
-
+cl, addr = s.accept()
+print("IP = " + str(wlan.ifconfig()[0]))
+print("Connected!\n")
+inpu = input("Receive or send[R/S]: ")
 
 def receivevalue():
     print("Receiving...")
-    s.send(b"GET Data") # Send request
-    ss=str(s.recv(512)) # Store reply
-    s.close()
+    cl.send(b"GET Data") # Send request
+    ss=str(cl.recv(512)) # Store reply
     time.sleep(0.2)
     return ss
 
 def sendvalue(val):
     print("Sending...")
-    try:
-        cl, addr = s.accept()
-        request = cl.recv(1024)
-        cl.send(str(val))
-        print("Sent " + str(val))
-        cl.close()
-    except OSError as e:
-        cl.close
-        print("Error Connection Terminated")
-   
-   
+    #request = cl.recv(1024)
+    cl.send(str(val))
+    print("Sent " + str(val))
+    
 if inpu == "R":
     value = str(receivevalue())
     print("Got " + value[1:])
 elif inpu == "S":
-    while True:
-        sendvalue("value")
-        break
+    sendvalue("value") 
 else:
     print("Invalid input. Please try again.")
-
